91b8ec63810b7aea91c95d87297d0edbdf0a6a02
Quan <trananhquan335@gmail.com>
2023-06-25 16:11:00 +0700

    btvn

--- src/main/java/com/example/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +29,10 @@ public class BlogController {
     }
 
     @GetMapping("/login")
-    public String getLoginPage() {
+    public String getLoginPage(Authentication authentication) {
+        if(authentication != null) {
+            return "redirect:/";
+        }
         return "security/login";
     }
 
